f4506be8a8b761426e089ba3c8d3c749cd140488
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-01-10 11:42:07 -0300

    adicionando camada de xadrez, e criando tabuleiro

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,14 @@
 package application;
 
+import chess.ChessMatch;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        ChessMatch CM = new ChessMatch();
+        UI.printBoard(CM.getPieces());
 
 
         sc.close();
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -9,7 +9,7 @@ public class Board {
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
-        pieces= new Piece[rows][columns];
+        pieces = new Piece[rows][columns];
     }
 
     public int getRows() {
@@ -27,4 +27,11 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
 }
