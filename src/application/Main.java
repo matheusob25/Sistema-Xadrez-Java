fadcdeb156d02f7e73c063a6c3191ac26eeb73c3
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-01-15 17:04:56 -0300

    adicionando peças capturadas em listas

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,17 +5,20 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatch CM = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printMatch(CM);
+                UI.printMatch(CM, captured);
                 System.out.println();
                 System.out.println("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -25,7 +28,12 @@ public class Main {
 
                 System.out.println("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
+
                 ChessPiece capturedPiece = CM.performChessMove(source, target);
+
+                if(capturedPiece != null){
+                    captured.add(capturedPiece);
+                }
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,11 +7,16 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 import javax.sql.ConnectionPoolDataSource;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChessMatch {
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
     public ChessMatch(){
         board = new Board(8,8);
         turn = 1;
@@ -53,6 +58,11 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+        if(capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
+
         return  capturedPiece;
     }
     private void validateSourcePosition(Position position){
@@ -78,6 +88,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
     private void initialSetup(){
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
