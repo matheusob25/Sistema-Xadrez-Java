38cc5eb9806938f29f02ad0e3ed0d9a8a5ade0c0
Matheus Oliveira <matheusoliveira0890@gmail.com>
2024-01-21 01:02:30 -0300

    jogada especial promoção

--- src/application/Main.java
+++ src/application/Main.java
@@ -34,6 +34,12 @@ public class Main {
                 if(capturedPiece != null){
                     captured.add(capturedPiece);
                 }
+                if(CM.getPromoted() != null){
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type =  sc.nextLine();
+                    CM.replacePromotedPiece(type);
+                }
+
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
